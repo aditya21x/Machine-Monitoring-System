a3190baf0e320b8fd83681d76341345d73ca8bf3
aditya21x <adityajindal1360@gmail.com>
2024-08-05 23:54:48 +0530

    Update MachineMonitoringSystem.ino

--- MachineMonitoringSystem.ino
+++ MachineMonitoringSystem.ino
@@ -1,16 +1,15 @@
 #include <LiquidCrystal.h>
 
-// Initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
-const int sensorPins[] = {9, 10, 11, 12, 13}; // Digital input pins
+const int sensorPins[] = {9, 10, 11, 12, 13}; 
 const int buzzerPin = 8;
-const int ledPins[] = {14, 15, 16, 17, 18}; // LED pins corresponding to each machine
+const int ledPins[] = {14, 15, 16, 17, 18}; 
 bool sensorValues[5];
 const char* machineIDs[] = {"MQASTC01", "MQASTC02", "MQASTC03", "MQASTC04", "MQASTC05"};
 
 void setup() {
-  lcd.begin(16, 2); // Set up the LCD's number of columns and rows
+  lcd.begin(16, 2); 
   pinMode(buzzerPin, OUTPUT);
   for (int i = 0; i < 5; i++) {
     pinMode(sensorPins[i], INPUT);
@@ -18,11 +17,11 @@ void setup() {
   }
   Serial.begin(9600);
   lcd.clear();
-  lcd.print("No Alert"); // Initial display
+  lcd.print("No Alert"); 
 }
 
 void loop() {
-  bool alert = false; // Variable to track if any alert is triggered
+  bool alert = false; 
 
   for (int i = 0; i < 5; i++) {
     sensorValues[i] = digitalRead(sensorPins[i]);
@@ -31,16 +30,16 @@ void loop() {
     Serial.print(": ");
     Serial.println(sensorValues[i]);
 
-    if (sensorValues[i] == HIGH) { // Check if the digital input is HIGH
+    if (sensorValues[i] == HIGH) { 
       digitalWrite(buzzerPin, HIGH);
       digitalWrite(ledPins[i], HIGH);
       lcd.clear();
-      lcd.setCursor(0, 0); // Set cursor to the beginning of the first row
+      lcd.setCursor(0, 0); 
       lcd.print("Alert:Machine ID");
-      lcd.setCursor(0, 1); // Set cursor to the beginning of the second row
+      lcd.setCursor(0, 1); 
       lcd.print(machineIDs[i]);
-      delay(1000); // Alert delay
-      alert = true; // An alert is present
+      delay(1000); 
+      alert = true; 
     } else {
       digitalWrite(ledPins[i], LOW);
     }
